cbe3de0521a2b64806bd195c9ffe81f576057af3
TUDG <tranduongtruong1623@gmail.com>
2023-05-09 13:12:57 +0700

    Fá»iristCommit

---
+++ src/System/Student.java
@@ -0,0 +1,12 @@
+package System;
+//this class contains information of a student including student ID, name, score, address, phone number, email,.
+public class Student {
+	private String name;
+    private int age;
+    private String studentId;
+    private String major;
+    private double gpa;
+    // add constructor, get, set , input and print student information
+    
+    
+}
---
+++ src/System/StudentGrade.java
@@ -0,0 +1,11 @@
+package System;
+// this class manage students' scores with specific subjects such as math, Literature, English, Physics.
+public class StudentGrade {
+	 // Fields
+    private int mathGrade;
+    private int literatureGrade;
+    private int englishGrade;
+    private int physicsGrade;
+    
+    //add constructor, get, set , input and print score information,
+}
